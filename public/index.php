e2ac957d18001d6fae041bd963a67d357e519dca
Sajad Torkamani <sajadtorkamani1@gmail.com>
2024-11-17 16:17:05 +0000

    wip

--- public/index.php
+++ public/index.php
@@ -1,3 +1,18 @@
-<?php
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Hello from Docker</title>
+  </head>
+  <body>
+    <h1>Hello from Docker!</h1>
 
-echo 'Hello from Docker!';
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. A ab accusamus
+      adipisci amet aperiam asperiores atque earum itaque magnam minus modi
+      nisi, nulla quaerat reiciendis saepe, sapiente vero! Praesentium,
+      vitae?
+    </p>
+  </body>
+</html>
+<?php
